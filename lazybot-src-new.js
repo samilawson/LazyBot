6d1bb263f7716c8ed4901ebab11e43066f2418f2
Sam Lawson <samilawson17@gmail.com>
2017-01-10 15:14:23 -0600

    minor

--- lazybot-src-new.js
+++ lazybot-src-new.js
@@ -91,6 +91,7 @@ bot.on("message", msg => {
       .setThumbnail(`${obj.REGION.FLAG}`)
       .addField('Embassies', obj.REGION.EMBASSIES[0].EMBASSY)
       .setFooter(`Generated on ${date} at ${time}`)
+        console.log(embed);
       msg.channel.sendEmbed(embed);
     })
   })
