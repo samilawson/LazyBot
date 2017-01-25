fdc4b1c064475b439442d2afd3c1d2eef6a08f22
Sam Lawson <samilawson17@gmail.com>
2017-01-24 19:17:18 -0600

    Update news.js

--- cmd/news.js
+++ cmd/news.js
@@ -3,20 +3,29 @@ const Discord = require("discord.js");
 let newsapi = new NewsAPI('0b3a687275104852a2b8e5c013dbc3b5');
 exports.run = (bot, msg, params = []) => {
   
-  var newsAgency;
-    if(params === "CNN"){
+   msg.reply('Please enter a valid news key: CNN, Washingon Post, WSJ (Wall Street Journal), google, espn, Reddit, or Reuters')
+  .then(() => {
+  msg.channel.awaitMessages(response => response.content === "CNN" || response.content === "Washington Post" || response.content === "WSJ" || response.content === "google" || response.content === "espn" || response.content === "Reddit" || response.content === "Reuters", {
+    max: 1,
+    time: 15000,
+    errors: ['time'],
+  })
+  .then((collected) => {
+    console.log(collected.first().content);
+    var newsAgency;
+    if(collected.first().content === "CNN"){
       newsAgency = "cnn";
-    } else if(params === "BBC"){
-      newsAgency = "bbc-news";
-    } else if(params === "CNBC"){
-      newsAgency = "cnbc";
-    } else if(params === "Google"){
+    } else if(collected.first().content === "Washington Post"){
+      newsAgency = "the_washington_post";
+    } else if(collected.first().content === "WSJ"){
+      newsAgency = "the_wall_street_journal";
+    } else if(collected.first().content === "google"){
       newsAgency = "google-news";
-    } else if(params === "ESPN"){
+    } else if(collected.first().content === "espn"){
       newsAgency = "espn";
-    } else if(params === "Reddit"){
+    } else if(collected.first().content === "Reddit"){
       newsAgency = "reddit-r-all";
-    } else if(params === "Reuters"){
+    } else if(collected.first().content === "Reuters"){
       newsAgency = "reuters";
     }
     const embed = new Discord.RichEmbed();
@@ -26,23 +35,25 @@ exports.run = (bot, msg, params = []) => {
   }).then(articlesResponse => {
     console.log(articlesResponse);
  
- 
       embed.setColor(3447003)
-      .setTitle(`Latest News for ${params}`)
-      .setThumbnail(`${articlesResponse.articles[0]["urlToImage"]}`)
-      .addField(`Top Headlines`, `[${articlesResponse.articles[0]["title"]}](${articlesResponse.articles[0]["url"]})`, true)
-       .addField(`\u200b`,`[${articlesResponse.articles[1]["title"]}](${articlesResponse.articles[1]["url"]})`, true)
-      .addField(`\u200b`,`[${articlesResponse.articles[2]["title"]}](${articlesResponse.articles[2]["url"]})`, true)
-      .addField(`\u200b`,`[${articlesResponse.articles[3]["title"]}](${articlesResponse.articles[3]["url"]})`, true)
-      .addField(`\u200b`,`[${articlesResponse.articles[4]["title"]}](${articlesResponse.articles[4]["url"]})`, true)
+      .setTitle(`Latest News for ${newsAgency}`)
+      .setThumbnail(`http://www.vtc.edu/sites/default/files/news-3.jpg`)
+      .addField(`Headline`, `${articlesResponse.articles[0]["title"]}`, true)
+      .addField(`Link`, `${articlesResponse.articles[0]["url"]}`, true)
+      .addField(`Headline`, `${articlesResponse.articles[1]["title"]}`, true)
+      .addField(`Link`, `${articlesResponse.articles[1]["url"]}`, true)
+      .addField(`Headline`, `${articlesResponse.articles[2]["title"]}`, true)
+      .addField(`Link`, `${articlesResponse.articles[2]["url"]}`, true)
+      .addField(`Headline`, `${articlesResponse.articles[3]["title"]}`, true)
+      .addField(`Link`, `${articlesResponse.articles[3]["url"]}`, true)
       msg.channel.sendEmbed(embed);
       
     })
-    
+    })
     .catch(() => {
       msg.channel.sendMessage('\:x: Oops! Something went wrong!');
     })
-
+})
 };
 
 exports.conf = {
