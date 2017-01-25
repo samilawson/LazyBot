f9ed0ea54bfcbe957ade0a005a4a279f26245709
Sam Lawson <samilawson17@gmail.com>
2017-01-24 18:57:28 -0600

    Update loadCommands.js

--- functions/core/loadCommands.js
+++ functions/core/loadCommands.js
@@ -19,6 +19,6 @@ module.exports = bot => {
     files.forEach(f => {
       delete require.cache[require.resolve(`../../cmd/${f}`)];
     });
-    bot.log(`Loaded ${c} commands, with ${a} aliases.`);
+    console.log(`Loaded ${c} commands, with ${a} aliases.`);
   });
 };
