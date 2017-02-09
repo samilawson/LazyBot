8e87ef1785767c910779beda50627d338e0af8cd
Sam Lawson <samilawson17@gmail.com>
2017-02-09 12:55:02 -0600

    Update servers.js

--- cmd/servers.js
+++ cmd/servers.js
@@ -1,1 +1,20 @@
+exports.run = (bot, msg, params = []) => {
+  msg.channel.sendMessage('On ' + bot.guilds.size.toLocaleString() + ' servers!');
 
+        
+    
+
+};
+
+exports.conf = {
+  enabled: true, // not used yet
+  guildOnly: false, // not used yet
+  aliases: [],
+  permLevel: 0 // Permissions Required, higher is more power
+};
+
+exports.help = {
+  name : "servers",
+  description: "Server count command.",
+  usage: "servers"
+};
