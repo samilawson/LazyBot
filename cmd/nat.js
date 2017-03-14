2a12099e908bb0b63909dae844bd10ef27222044
Sam Lawson <samilawson17@gmail.com>
2017-03-14 15:07:54 -0500

    minor edits

--- cmd/nat.js
+++ cmd/nat.js
@@ -10,19 +10,21 @@ exports.run = (bot, msg, params = []) => {
   const now = new Date();
    const date = moment(now).format("MMM/DD/YYYY"); //This is better. 
   const time = moment(now).format("H:mm:ss");
-  const result = request.get(`https://www.nationstates.net/cgi-bin/api.cgi?nation=${name}&q=name+gdp+population+region+wa+flag+fullname+influence+census;mode=score;scale=66`);
+   const result = request.get(`https://www.nationstates.net/cgi-bin/api.cgi?nation=${name}&q=name+gdp+population+currency+animal+region+wa+flag+fullname+motto+influence+census;mode=score;scale=66`);
 
   result.then((res) => {
     parseString(res.text, (err, obj) => {
       embed.setColor(3447003)
         .setAuthor(`${obj.NATION.NAME}`, `${obj.NATION.FLAG}`)
-        .setTitle(`Nation Info for ${obj.NATION.NAME}`)
-        .setDescription(obj.NATION.FULLNAME)
+        .setTitle(`Nation Info for ${obj.NATION.FULLNAME}`)
+        .setDescription(`${obj.NATION.MOTTO}`)
         .setThumbnail(`${obj.NATION.FLAG}`)
         .addField('Region', obj.NATION.REGION, true)
         .addField('Influence', obj.NATION.INFLUENCE, true)
         .addField('Population', obj.NATION.POPULATION * 10000000, true)
         .addField('Economy', "$" + obj.NATION.GDP, true)
+        .addField('Currency', `${obj.NATION.CURRENCY}`, true)
+        .addField('Animal', `${obj.NATION.ANIMAL}`, true)
         .addField('WA Status?', obj.NATION.UNSTATUS, true)
         .addField('Endorsement Count', Math.round(obj.NATION.CENSUS[0].SCALE[0].SCORE), true)
         .addField('Link', "http://www.nationstates.net/nation=" + name)
