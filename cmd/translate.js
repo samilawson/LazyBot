1696533d055e2206ff22d34275dd6d2832292a51
Sam Lawson <samilawson17@gmail.com>
2017-03-28 09:55:35 -0500

    Update translate.js

--- cmd/translate.js
+++ cmd/translate.js
@@ -12,8 +12,8 @@ const langTo = params[0];
 var toTranslate = params.slice(1).join(" ");
  console.log(langTo);
  console.log(toTranslate);
-googleTranslate.translate(toTranslate, langTo, function(err, translation){
- console.log(translation[0]);
+googleTranslate.translate(toTranslate, langTo, function(err, translated){
+ console.log(translated.translatedText);
  embed.setColor(3447003)
  .addField('', '```\n' + translation[0] + '\n ```')
 msg.channel.sendEmbed(embed); 
