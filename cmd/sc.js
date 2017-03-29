a484d01a8b50edf27d23d5b1f0e17b78f6a5a616
Sam Lawson <samilawson17@gmail.com>
2017-03-29 09:13:49 -0500

    Create sc.js

---
+++ cmd/sc.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const moment = require("moment");
+require("moment-duration-format");
+var request = require('superagent');
+var parseString = require('xml2js').parseString;
+var xml2js = require('xml2js');
+exports.run = (bot, msg, params = []) => {
+const embed = new Discord.RichEmbed();
+  const now = new Date();
+   const date = moment(now).format("MMM/DD/YYYY"); //This is better. 
+  const time = moment(now).format("H:mm:ss");
+
+request.get('https://www.nationstates.net/cgi-bin/api.cgi?wa=2&q=lastresolution').end((err, res) => {
+        console.log(res.text);
+        var second = res.text.replace(/(<([^>]+)>)/ig,"");
+        console.log(second);
+        msg.channel.sendMessage(second);
+      });
+
+};
+
+
+exports.conf = {
+  enabled: true, // not used yet
+  guildOnly: false, // not used yet
+  aliases: [""],
+  permLevel: 0 // Permissions Required, higher is more power
+};
+
+exports.help = {
+  name : "sc",
+  description: "Gives the last Security Council Resolution that was voted on.",
+  usage: "sc"
+};
