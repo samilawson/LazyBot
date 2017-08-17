2cf2f27745e90ff4592c17c9acdd5c2c620dce6b
Sam Lawson <samilawson17@gmail.com>
2017-08-17 09:02:29 -0500

    Update and rename lazybot-src-new.js to index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
+const fs = require("fs");
+
+const client = new CommandoClient({
+    commandPrefix: '--',
+    owner: '213251218154192896',
+    disableEveryone: true
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['ns', 'NationStates'],
+        ['fun', 'Fun'],
+        ['util', 'Utilities'],
+        ['moderation', 'Moderation']
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+fs.readdir(`${__dirname}/events/`, (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const eventFunction = require(`${__dirname}/events/${file}`);
+    const eventName = file.split('.')[0];
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+});
+});
+
+    client.on('ready', () => {
+    console.log('Logged in!');
+    client.user.setGame('--help | --invite');
+});
+
+
+    client.login('tokenbehere');
+
+
+   
